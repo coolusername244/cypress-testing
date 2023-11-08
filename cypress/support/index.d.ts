2bab70d6edc5e2eb51630cfc6bef5b490f24f84b
Lee <leesheppard2404@gmail.com>
2023-11-08 11:29:56 +0100

    fix: added file to add types of new commands

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,16 @@
+export {};
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      visitHomePage(): Chainable<any>;
+      performSignUp(
+        name: string,
+        email: string,
+        password: string,
+      ): Chainable<any>;
+      performLogin(email: string, password: string): Chainable<any>;
+      deleteAccount(): Chainable<any>;
+    }
+  }
+}
